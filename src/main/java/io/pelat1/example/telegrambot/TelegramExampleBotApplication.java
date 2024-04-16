af39ea0255d8f2c2aa9af7dc40d069c12b21bc85
Pelat-1 <nicolo.rogai@outlook.com>
2024-04-16 10:57:02 +0200

    Spring Boot initializr

---
+++ src/main/java/io/pelat1/example/telegrambot/TelegramExampleBotApplication.java
@@ -0,0 +1,13 @@
+package io.pelat1.example.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramExampleBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramExampleBotApplication.class, args);
+	}
+
+}
